c95ecfb869ecc861853d1328fa4dbf25ce65a988
castlebean100 <71800772+castlebean100@users.noreply.github.com>
2020-12-21 15:59:14 +0900

    create
    create

---
+++ src/main/java/com/wb/sb/WhiteBeanApplication.java
@@ -0,0 +1,13 @@
+package com.wb.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteBeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wb/sb/WhiteBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wb.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
