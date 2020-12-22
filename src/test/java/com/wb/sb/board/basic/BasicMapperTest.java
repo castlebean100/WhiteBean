03b89f68af0587444caaa2b07b497303a250288e
castlebean100 <71800772+castlebean100@users.noreply.github.com>
2020-12-22 16:14:29 +0900

    BasicUpdate
    UpdateTest_check

--- src/test/java/com/wb/sb/board/basic/BasicMapperTest.java
+++ src/test/java/com/wb/sb/board/basic/BasicMapperTest.java
@@ -13,15 +13,30 @@ class BasicMapperTest {
 	@Autowired
 	private BasicMapper BasicMapper;
 	
-	@Test
+	//@Test
 	void setInsertTest() throws Exception {
 		BasicVO basicVO = new BasicVO();
-		basicVO.setTitle("test title");
-		basicVO.setContents("test contents");
-		
+		basicVO.setTitle("123");
+		basicVO.setContents("123");
+		 
 		int result = BasicMapper.setInsert(basicVO);
 	
 		assertNotEquals(0, result);
 	}
+	
+	
+	@Test
+	void setUpdateTest() throws Exception {
+		BasicVO basicVO = new BasicVO();
+		basicVO.setTitle("tttteeesssstttt");
+		basicVO.setContents("tttteeessssttt");
+		basicVO.setNum(9);
+		
+		int result = BasicMapper.setUpdate(basicVO);
+		
+		assertNotEquals(0, result);
+	}
+	
+	
 
 }
