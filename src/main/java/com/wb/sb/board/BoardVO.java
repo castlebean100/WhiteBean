eb94ed61e9b45b86f6e4ad8b46f8ec61c92d09b5
castlebean100 <71800772+castlebean100@users.noreply.github.com>
2020-12-22 14:12:39 +0900

    Create BoardVO.java

---
+++ src/main/java/com/wb/sb/board/BoardVO.java
@@ -0,0 +1,16 @@
+package com.wb.sb.board;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+
+	private long num;
+	private String title;
+	private String writer;
+	private String contents;
+	private Date regDate;
+	private long hit;
+}
